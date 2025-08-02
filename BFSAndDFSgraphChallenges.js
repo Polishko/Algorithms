787e476c110f8f4b697dbea27df1674fc4433a45
Niya Mateeva Yondzhju <119063181+Polishko@users.noreply.github.com>
2025-08-02 07:11:46 +0300

    Update BFSAndDFSgraphChallenges.js
    Added alternative for ch.1

--- BFSAndDFSgraphChallenges.js
+++ BFSAndDFSgraphChallenges.js
@@ -1,32 +1,64 @@
 // 1. Find the number of islands where 1 represents land and 0 represents water
-// Initial solution
+
+// Alternative
 const numberOfIslands = (grid) => {
-  const visited = new Set();
+  const visited = Array(grid.length)
+    .fill()
+    .map((_, i) => Array(i.length).fill(false));
   let islands = 0;
-
-  const traverse = (r, c) => {
-    if (r < 0 || r > grid.length - 1 || c < 0 || c > grid[0].length - 1) return;
-    if (grid[r][c] === "0" || visited.has(`${r},${c}`)) return;
-
-    visited.add(`${r},${c}`);
-
-    traverse(r, c + 1);
-    traverse(r + 1, c);
-    traverse(r, c - 1);
-    traverse(r - 1, c);
-  };
 
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j] === "1" && !visited.has(`${i},${j}`)) {
+      if (grid[i][j] === "1" && !visited[i][j]) {
         islands++;
-        traverse(i, j);
+        traverse(i, j, grid, visited);
       }
     }
   }
 
   return islands;
 };
+
+const traverse = (r, c, grid, visited) => {
+  if (r < 0 || r > grid.length - 1 || c < 0 || c > grid[0].length - 1) return;
+  if (grid[r][c] === "0" || visited[r][c]) return;
+
+  visited[r][c] = true;
+
+  traverse(r, c + 1, grid, visited);
+  traverse(r + 1, c, grid, visited);
+  traverse(r, c - 1, grid, visited);
+  traverse(r - 1, c, grid, visited);
+};
+
+// Initial solution
+// const numberOfIslands = (grid) => {
+//   const visited = new Set();
+//   let islands = 0;
+
+//   const traverse = (r, c) => {
+//     if (r < 0 || r > grid.length - 1 || c < 0 || c > grid[0].length - 1) return;
+//     if (grid[r][c] === "0" || visited.has(`${r},${c}`)) return;
+
+//     visited.add(`${r},${c}`);
+
+//     traverse(r, c + 1);
+//     traverse(r + 1, c);
+//     traverse(r, c - 1);
+//     traverse(r - 1, c);
+//   };
+
+//   for (let i = 0; i < grid.length; i++) {
+//     for (let j = 0; j < grid[0].length; j++) {
+//       if (grid[i][j] === "1" && !visited.has(`${i},${j}`)) {
+//         islands++;
+//         traverse(i, j);
+//       }
+//     }
+//   }
+
+//   return islands;
+// };
 
 // test output
 // const grid = [
